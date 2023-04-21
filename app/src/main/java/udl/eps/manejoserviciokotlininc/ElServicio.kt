249f99f306c68ad11fc933a8008b715deba5656e
Ant <antoniolopez18022002@gmail.com>
2023-04-21 19:42:20 +0200

    done

--- app/src/main/java/udl/eps/manejoserviciokotlininc/ElServicio.kt
+++ app/src/main/java/udl/eps/manejoserviciokotlininc/ElServicio.kt
@@ -3,34 +3,53 @@ package udl.eps.manejoserviciokotlininc
 import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
+import android.os.Binder
 import android.os.IBinder
 import android.widget.Toast
 
 class ElServicio: Service() {
 
-    private var player: MediaPlayer? = null
+    private var soundPlayer: MediaPlayer? = null
+    private var songPlayer: MediaPlayer? = null
+    private val iBinder:IBinder = MyBinder()
 
-    override fun onBind(p0: Intent?): IBinder? {
-        TODO("Not yet implemented")
+    class MyBinder : Binder() {
+        fun getService(): ElServicio{
+            return ElServicio()
+        }
+    }
+
+    override fun onBind(p0: Intent?): IBinder {
+        return iBinder
     }
 
     override fun onCreate() {
         super.onCreate()
         Toast.makeText(this, R.string.creaserv, Toast.LENGTH_LONG).show()
-        player = MediaPlayer.create()
-        player!!.isLooping = true
+        soundPlayer = MediaPlayer.create(this, R.raw.train)
+        soundPlayer!!.isLooping = true
+        songPlayer = MediaPlayer.create(this, R.raw.song)
+        songPlayer!!.isLooping = true
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         super.onStartCommand(intent, flags, startId)
-        Toast.makeText(this, R.string.iniserv, Toast.LENGTH_LONG).show()
-
+        val mode: String? = intent?.extras?.getString("mode")
+        if (mode == "Sound"){
+            Toast.makeText(this, R.string.iniservSound, Toast.LENGTH_LONG).show()
+            soundPlayer?.start()
+        }
+        else if (mode == "Song"){
+            Toast.makeText(this, R.string.iniservSong, Toast.LENGTH_LONG).show()
+            songPlayer?.start()
+        }
         return startId
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        if (soundPlayer?.isPlaying == true)soundPlayer!!.stop()
+        if (songPlayer?.isPlaying == true)songPlayer!!.stop()
         Toast.makeText(this, R.string.finaserv, Toast.LENGTH_LONG).show()
-
     }
 }
--- app/src/main/java/udl/eps/manejoserviciokotlininc/MainActivity.kt
+++ app/src/main/java/udl/eps/manejoserviciokotlininc/MainActivity.kt
@@ -1,30 +1,65 @@
 package udl.eps.manejoserviciokotlininc
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import udl.eps.manejoserviciokotlininc.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() , View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
+    var mService: ElServicio? = null
+    var mIsBound: Boolean? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnIn.setOnClickListener(this)
+        binding.btnInSound.setOnClickListener(this)
+        binding.btnInSong.setOnClickListener(this)
         binding.btnFin.setOnClickListener(this)
 
+        this.registerReceiver(MyBroadcast(), IntentFilter(Intent.ACTION_HEADSET_PLUG))
     }
 
     override fun onClick(src: View) {
-
         when(src.id) {
-            R.id.btnIn -> startService(intent)
-            R.id.btnFin -> stopService(intent)
+            R.id.btnInSound -> startBroadcast("Sound")//startService("Sound")
+            R.id.btnInSong -> startBroadcast("Song")//startService("Song")
+            R.id.btnFin -> stopBroadcast()//stopService()
+        }
+    }
+    private fun startBroadcast(mode: String){
+        val serviceIntent = Intent(this, MyBroadcast::class.java)
+        serviceIntent.putExtra("mode", mode)
+        sendBroadcast(serviceIntent)
+        bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE)
+    }
+
+    private fun stopBroadcast(){
+        val serviceIntent = Intent(this, MyBroadcast::class.java)
+        serviceIntent.putExtra("mode", "Stop")
+        sendBroadcast(serviceIntent)
+        unbindService(serviceConnection)
+    }
+
+    private val serviceConnection: ServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName, iBinder: IBinder) {
+            // We've bound to MyService, cast the IBinder and get MyBinder instance
+            val binder: ElServicio.MyBinder = iBinder as ElServicio.MyBinder
+            mService = binder.getService()
+            mIsBound = true
+        }
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            mIsBound = false
         }
     }
 }
